4de828745497c1847beccafbe45a98381f7c610f
SidddharthTotla <67379076+SidddharthTotla@users.noreply.github.com>
2020-07-16 21:31:35 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,16 @@
+class box{
+    constructor(x,y,width,height){
+        var options = {
+            restitution:0.8,
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        World.add(world.this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill(255,0,0);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}   
+ 
